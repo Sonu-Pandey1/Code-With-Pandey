b8ce1a1436d45516ec9d87ddd0cb742a79a03c1e
sonu <sonupandeybaldeo2003@gmail.com>
2023-08-04 00:35:44 +0530

    gn

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -31,14 +31,15 @@ export default function Navbar() {
             </div> */}
 
 
-            <nav className="navbar navbar-expand-lg  p-4">
+            <nav className="navbar navb  navbar-expand-lg  p-4">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">CodeWithPandey</a>
+                    {/* <a className="navbar-brand" href="#">CodeWithPandey</a> */}
+                    <NavLink className="navbar-brand font-color-700 title" to="/">CodeWithPandey</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav navul ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
                                 <NavLink className="nav-link" to="/">Home</NavLink>
@@ -64,10 +65,55 @@ export default function Navbar() {
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/workwithus">Contact With Us</NavLink>
                             </li>
-                            <li className=" d-flex justify-content-evenly navbarbtns">
-                                <button className="px-1  mx-2 ggg "><BsFillMoonStarsFill className="libtnmoon"/></button>
-                                <button className=" ">Login</button>
-                                <button className=" ">SignUp</button>
+                            <li className=" d-flex justify-content-evenly navbarbtnli">
+                                <button className="px-1  mx-2 darkmodeIcon "><BsFillMoonStarsFill className="libtnmoon"/></button>
+                                <button className="navbarbtn mx-2 btn ">Login</button>
+                                <button className="navbarbtn btn ">SignUp</button>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                </div>
+            </nav>
+
+            <nav className="navbar    navbar-expand-lg  p-4">
+                <div className="container-fluid">
+                    {/* <a className="navbar-brand" href="#">CodeWithPandey</a> */}
+                    <NavLink className="navbar-brand font-color-700 title" to="/">CodeWithPandey</NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav navul ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
+                                <NavLink className="nav-link" to="/">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link"  to="/courses ">Courses</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/tutorial">Tutorial</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/blog">Blog</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/notes">Notes</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/mygear">My Gear</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/workwithus">Contact With Us</NavLink>
+                            </li>
+                            <li className=" d-flex justify-content-evenly navbarbtnli">
+                                <button className="px-1  mx-2 darkmodeIcon "><BsFillMoonStarsFill className="libtnmoon"/></button>
+                                <button className="navbarbtn mx-2 btn ">Login</button>
+                                <button className="navbarbtn btn ">SignUp</button>
                             </li>
                         </ul>
                         
